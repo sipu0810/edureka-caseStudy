79210fae7b42492af1f58fd03c5b6f425a38544b
shipra swaroop <shipra.ambasta8@gmail.com>
2019-10-21 09:00:30 +0000

    modify

--- create.sh
+++ create.sh
@@ -1,16 +1,6 @@
 #! /bin/bash
 
-# git-create-branch <release>
- 
-if [ $# -ne 1 ]
-then
-	echo 1>&2 "Usage: $0 release"
-	exit 127
-fi
- 
-branch_name=$1
-git push origin master:refs/heads/$release
-echo "git push origin master:refs/heads/$release"
-git fetch origin
-git checkout --track -b $release origin/$release
-git pull
+# git-create-branch 
+git checkout -b release
+vi index.html
+git commit -m "Made the change.." index.html
